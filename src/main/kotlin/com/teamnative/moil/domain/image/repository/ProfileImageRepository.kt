a5103b7f7ecd18c120a9c66509ea45629b68913e
soyuo <tasexyz@gmail.com>
2026-08-14 11:24:14 +0900

    feat: 프로필 이미지 도메인 기본 구조 추가

---
+++ src/main/kotlin/com/teamnative/moil/domain/image/repository/ProfileImageRepository.kt
@@ -0,0 +1,16 @@
+package com.teamnative.moil.domain.image.repository
+
+import com.teamnative.moil.domain.image.model.ProfileImage
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProfileImageRepository : JpaRepository<ProfileImage, Long> {
+    fun findByKey(key: String): ProfileImage?
+
+    fun findByUserId(userId: Long): ProfileImage?
+
+    fun existsByKey(key: String): Boolean
+
+    fun deleteByUserId(userId: Long)
+}
